cbfc7eed9c5a42c80346b64e85287dd71f380c2e
faliss <gaal.faliss@gmail.com>
2025-09-19 15:56:42 -0500

    rsa key generated
    kid and expiry timestamp added

--- jwks.py
+++ jwks.py
@@ -7,3 +7,11 @@ import base64
 
 app = Flask(__name__)
 
+key = {}
+
+def generate_rsa_key(expiry_minutes=60):
+  private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+  )
+  public_key = private_key.public_key()
